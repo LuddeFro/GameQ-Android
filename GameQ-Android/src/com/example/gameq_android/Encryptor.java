8ab162c9e6fa9bdda2feab37f6471a98315fcb34
Ibz <ibz@Ibzs-MacBook-Pro.local>
2014-04-28 18:17:04 +0200

    Added SHA256 hashing functionallity. First commit, gege.

--- GameQ-Android/src/com/example/gameq_android/Encryptor.java
+++ GameQ-Android/src/com/example/gameq_android/Encryptor.java
@@ -1,5 +1,8 @@
 package com.example.gameq_android;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 /**
  * Used to Hash not encrypt passwords
@@ -8,14 +11,33 @@ package com.example.gameq_android;
  * 
  */
 public class Encryptor{
-
-	
-	
-	public String HashSHA256(String string) {
-		
-		//TODO also edit md5 in iOS and OSX to sha256
-		return null;
+	/**
+	 * HashSHA256
+	 * @param password the password that should be hashed.
+	 * @return a hashed hex encoded version of the string, returns null in case of error.
+	 */
+	public static String HashSHA256(String password) throws NoSuchAlgorithmException {
+        try {
+        	MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(password.getBytes());
+ 
+			byte byteData[] = md.digest();
+			return bytesToHex(byteData);
+        } catch (NoSuchAlgorithmException e) {
+        	System.out.println("Error: " + e);
+        	return null;
+        }
 	}
 	
-	
+	//TODO Make sense of this function
+   private static String bytesToHex(byte[] b) {
+      char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                         '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+      StringBuffer buf = new StringBuffer();
+      for (int j=0; j<b.length; j++) {
+         buf.append(hexDigit[(b[j] >> 4) & 0x0f]);
+         buf.append(hexDigit[b[j] & 0x0f]);
+      }
+      return buf.toString();
+   }
 }
